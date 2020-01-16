684dc23b743adb948a83cff9a6974a6b7245d848
abxjdorn <abxjdorn@users.noreply.github.com>
2020-01-15 18:08:33 -0700

    multiprocessing

--- echo_server.py
+++ echo_server.py
@@ -1,22 +1,31 @@
-import time, socket
+import time, socket, multiprocessing
 
 HOST = 'localhost'
 PORT = 8001
 
-with socket.socket() as s:
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((HOST, PORT))
-    s.listen(2)
-
+def handle_request(conn, addr):
+    message = b''
     while True:
-        conn, addr = s.accept()
-        print(conn, addr)
-        message = b''
+        data = conn.recv(4096)
+        time.sleep(0.5)
+        message += data
+        conn.sendall(data)
+        if not data: break
+    print(message)
+    conn.close()
+
+def main():
+    multiprocessing.set_start_method('fork')
+    with socket.socket() as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST, PORT))
+        s.listen(1)
+
         while True:
-            data = conn.recv(4096)
-            time.sleep(0.5)
-            message += data
-            conn.sendall(data)
-            if not data: break
-        print(message)
-        conn.close()
+            conn, addr = s.accept()
+            print(conn, addr)
+            p = multiprocessing.Process(
+                target=handle_request, args=(conn, addr))
+            p.start()
+
+main()
--- proxy_server.py
+++ proxy_server.py
@@ -1,26 +1,36 @@
-import time, socket
+import socket, multiprocessing
 
 LOCAL_ADDRESS = 'localhost'
 LOCAL_PORT = 8001
 REMOTE_ADDRESS = 'www.google.com'
 REMOTE_PORT = 80
 
-with socket.socket() as inside:
-    inside.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    inside.bind((LOCAL_ADDRESS, LOCAL_PORT))
-    inside.listen(2)
-
+def handle_request(conn, addr):
+    outside = socket.create_connection((REMOTE_ADDRESS, REMOTE_PORT))
     while True:
-        conn, addr = inside.accept()
-        outside = socket.create_connection((REMOTE_ADDRESS, REMOTE_PORT))
+        data = conn.recv(4096)
+        outside.sendall(data)
+        if not data: break
+    outside.shutdown(socket.SHUT_WR)
+    while True:
+        data = outside.recv(4096)
+        conn.sendall(data)
+        if not data: break
+    outside.close()
+    conn.shutdown(socket.SHUT_WR)
+    conn.close()
+
+def main():
+    multiprocessing.set_start_method('fork')
+    with socket.socket() as inside:
+        inside.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        inside.bind((LOCAL_ADDRESS, LOCAL_PORT))
+        inside.listen(1)
+
         while True:
-            data = conn.recv(4096)
-            outside.sendall(data)
-            if not data: break
-        outside.shutdown(socket.SHUT_WR)
-        while True:
-            data = outside.recv(4096)
-            conn.sendall(data)
-            if not data: break
-        outside.close()
-        conn.close()
+            conn, addr = inside.accept()
+            p = multiprocessing.Process(
+                target=handle_request, args=(conn, addr))
+            p.start()
+
+main()
